0262a5c10348736ae7d037dfd0fb9e6adb88accc
S-P-H-E <sphe.g.personal@gmail.com>
2025-10-22 14:00:05 +0200

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native"
+
+export default function Index() {
+  return (
+    <View className="flex-1 gap-1 justify-center items-center bg-black">
+      <Text className="text-white font-semibold text-2xl">Empty Expo Template By Sphe.</Text>
+      <Text className="text-[var(--description)] font-semibold text-lg">(With Nativewind / TailwindCSS)</Text>
+    </View>
+  )
+}
